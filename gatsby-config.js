3d699aef36c80632024607a426ae28107eb26d20
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-05-20 11:59:51 -0400

    chore: update site configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -102,8 +102,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `amgr_dev`,
+        short_name: `amgr`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
