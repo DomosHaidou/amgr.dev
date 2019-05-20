f163bcfa2ad276e2a6a469781a0ecca8c89ce5e6
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-05-20 11:51:58 -0400

    feat: replace logo and styling

--- gatsby-config.js
+++ gatsby-config.js
@@ -108,7 +108,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/logo-transparent.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
