e947d6d7b004de956d3570b0c8db58fd5754b297
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-06-17 06:26:05 -0400

    feat: add quote and book details components

--- src/components/book-details.js
+++ src/components/book-details.js
@@ -1,16 +1,21 @@
 import React from "react"
+import remark from 'remark';
+import remarkHtml from 'remark-html';
 
 class BookDetails extends React.Component{
     render(){
+        let content = remark()
+          .use(remarkHtml)
+          .processSync(this.props.children).toString();
         return (
           <div class="book-details">
               <div class="row">
                   <div class="col-3">
-                    <img src={this.props.image} />
+                    <a target="_blank" href={this.props.link}><img src={this.props.image} /></a>
                   </div>
                   <div class="col-9">
                     <h4>{this.props.title}</h4>
-                    <p>{this.props.description}</p>
+                    <div dangerouslySetInnerHTML={{__html: content }} />
                   </div>
               </div>
 
